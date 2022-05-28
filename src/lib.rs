46674afce2dfc03ef565e8a101f77ef03d3e0bf4
Joel Montes de Oca <6587811+JoelMon@users.noreply.github.com>
2022-05-28 17:46:00 -0400

    began work on converting is_even() to use genetics

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,25 @@
+/// is_even returns true when given an int is given with an even polarity.
+pub fn is_even<T>(n: T) -> bool {
+    if n % 2 == 0 {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use crate::is_even;
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn t_is_even() {
+        let even_numbers = [2, 4, 8, 10, 100, 5_000, -5_000, -100, -8];
+
+        for n in even_numbers {
+            let result = is_even(n);
+            assert_eq!(result, true);
+        }
+
+        let small_num = 2i8;
+        let result = is_even(small_num);
     }
 }
